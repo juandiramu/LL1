f1f29c292e524a7f43f5683dda9ecf5abcfc2d12
Juan Diego Ramírez Muñoz <juan.1702011618@ucaldas.edu.co>
2022-10-05 20:46:35 -0500

    Agregando cositas

--- RecursionIzquierda.py
+++ RecursionIzquierda.py
@@ -1,7 +1,9 @@
+from errno import EDEADLK
 from lib2to3.pgen2.token import EQEQUAL
 from operator import length_hint
 import string
 from this import d
+from tkinter import E
 
 
 class RecursionIzquierda:
@@ -9,16 +11,27 @@ class RecursionIzquierda:
         self.lista=[{}]
 
     def hallarRecursion(self,key,diccionario):
-        for i in range(len(diccionario)) :
-            if (key== diccionario[i]) and(diccionario[i-1]=="|" or i==0):
-                print("Hay recursion izquierda" +diccionario[i])        
+        for i in range(len(diccionario)):
+            if key==diccionario[i] and( diccionario[i-1]=="|" or i==0):
+                print("hay recursion en:"+key)
+            
 
 
     def  recursion(self,lista):
-        for  lista in lista:
-            for key in lista:
-                print(key)
-                #self.hallarRecursion(key,lista[key])
+        self.imprimir(lista)
+        
+        for  lista1 in lista:
+            for key in lista1:
+                self.hallarRecursion(key,lista1[key])
+                
+    def imprimir(self,lista):
+        for list in lista:
+            for key in list:
+                print(key+"->",end="")
+                for list in list[key]:
+                    print(list,end="")
+                print()
+
                 
 
     
