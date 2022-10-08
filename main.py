1a38b8a298a857cb3131be95290c237f7583696c
Juan Diego Ramírez Muñoz <juan.1702011618@ucaldas.edu.co>
2022-10-08 15:59:18 -0500

    Primeros ya funcionando

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from primeros import Primeros
 if __name__ == "__main__":
     re= RecursionIzquierda()
     primeros=Primeros();
-    lista={"LE":["LE","R","|","F","|","R"],
+    lista={"LE":["R","|","F","|","R"],
     "E":["s","*","l","E"],
-    "F":["4","|","6","|","F"],
+    "F":["4","|","6",],
     "R":["i","|","E","F"]}
     terminales=["s","*","l","4","6","i"]
     noTerminales=["LE","F","R","E"]
--- primeros.py
+++ primeros.py
@@ -3,25 +3,28 @@ class Primeros:
         self.lista=[{}]
 
     def hallarPrimeros(self,diccionario):
-        for dic in diccionario.values():
-            self.mostrar(dic,diccionario)
-            print(dic)
-            
-    def mostrar(self,dic,diccionario):
         lista=[]
-        bandera=True
-        for i in range(len(dic)):
-            if i==0:
-                for key in diccionario.keys():
-                    if dic[i]==key:
-                        bandera=False;
-                if bandera:
-                    lista.append(dic[i])
-                    print(dic[i]+" Es un primero")
-            if i!=0 and dic[i-1]=="|":
-                for key in diccionario.keys():
-                    if dic[i]==key:
-                        bandera=False;
-                if bandera:
-                    lista.append(dic[i])
-                    print(dic[i]+" Es un primero")
+        for dic in diccionario.values():
+            print(dic)
+            self.mostrar(dic,diccionario,lista)
+            print(lista)
+            lista=[]
+
+            
+    def mostrar(self,dic,diccionario,lista):
+            bandera=True
+            for i in range(len(dic)):
+                if i==0:
+                    for key in diccionario.keys():
+                        if dic[i]==key:
+                            bandera=False;
+                            self.mostrar(diccionario[key],diccionario,lista)
+                    if bandera:
+                        lista.append(dic[i])
+                if i!=0 and dic[i-1]=="|":
+                    for key in diccionario.keys():
+                        if dic[i]==key:
+                            bandera=False;
+                            self.mostrar(diccionario[key],diccionario,lista)
+                    if bandera:
+                        lista.append(dic[i])
